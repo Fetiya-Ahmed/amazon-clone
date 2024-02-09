9f9e6ec3ff62e878e9e3fc09e9b63d96b4f3f458
Fetiya-Ahmed <fetiyaebrahim74@gmail.com>
2024-02-08 21:30:38 -0600

    pages,api,router commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
-import Landing from './Pages/Landing/Landing';
+// import Landing from './Pages/Landing/Landing';
 // import Header from "./Components/Header/Header"
+import Routing from './Router';
 
 
 
 function App() {
   return (
     <div className="App">
-    <Landing/>
+      {/* <Landing/> */}
+      <Routing/>
     </div>
   );
 }
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import CarouselEffect from "../../Components/Carousel/Carousel";
 import Category from "../../Components/Category/Category";
 import LayOut from "../../Components/LayOut/LayOut";
-// import Product from "../../Components/Products/Product";
+import Product from "../../Components/Products/Product";
 
 function Landing() {
   return (
     <LayOut>
       <CarouselEffect />
       <Category />
-      {/* <Product /> */}
+      <Product />
     </LayOut>
   );
 }
